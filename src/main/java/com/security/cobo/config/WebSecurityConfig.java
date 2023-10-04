e40498f055ceb9fcb3e119f9ec27a3952880b48b
Jose Cobo <cobogue@gmail.com>
2023-10-04 09:31:11 +0000

    test

--- src/main/java/com/security/cobo/config/WebSecurityConfig.java
+++ src/main/java/com/security/cobo/config/WebSecurityConfig.java
@@ -4,33 +4,44 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 class WebSecurityConfig {
-	 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    
-        return http
-	        .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
-	        .build();
-     
-    }
 
      @Bean
      CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setMaxAge((long) 365 * 24 * 60 * 60);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", configuration);
+        source.registerCorsConfiguration("*/**", configuration);
         return source;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        http
+        .csrf(csrf -> csrf.disable())
+        //.cors((cors) -> cors.configurationSource(corsConfigurationSource()))
+        .cors(cors -> cors.disable())
+        .authorizeHttpRequests((authz) -> authz
+             .requestMatchers(HttpMethod.POST, "/login").permitAll()
+             .anyRequest().authenticated()
+        );
+    
+        return http.build();
+     
     }
 }
